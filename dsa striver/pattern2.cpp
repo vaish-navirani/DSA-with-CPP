9c5b04a633e905a6abf2814487d48e485a1178d5
vaish-navirani <2105764@kiit.ac.in>
2025-04-27 12:48:08 +0530

    pattern

---
+++ dsa striver/pattern2.cpp
@@ -0,0 +1,19 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    for(int row=1;row<=5;row++)
+    {
+        for(int col=1;col<=row;col++)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+        
+    }
+
+    return 0;
+}
+}
---
+++ dsa striver/pattern3.cpp
@@ -0,0 +1,18 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    for(int row=1;row<=5;row++)
+    {
+        for(int col=1;col<=5;col++)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+        
+    }
+
+    return 0;
+}
